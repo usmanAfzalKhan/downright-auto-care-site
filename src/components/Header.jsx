05c0e3b676c0d09fdba2d1cceae4bebec60edcce
usmanAfzalKhan <usmanafzal.khan2001@gmail.com>
2025-06-22 20:35:22 -0400

    feat: import and display logo.png in Header with set height and objectFit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
 // src/components/Header.jsx
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import logo from '../assets/images/logo.png'  // adjust path if needed
 
 export default function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
         <div className="container">
-          {/* Logo placeholder */}
-          <Link className="navbar-brand fw-bold" to="/">
-            Downright Auto Care
+          {/* Logo */}
+          <Link to="/" className="navbar-brand">
+            <img 
+              src={logo} 
+              alt="Downright Auto Care" 
+              height={60} 
+              style={{ objectFit: 'contain' }} 
+            />
           </Link>
+
           {/* Mobile toggle */}
           <button
             className="navbar-toggler"
@@ -23,32 +30,24 @@ export default function Header() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+
+          {/* Nav links */}
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
-                <NavLink end to="/" className="nav-link">
-                  Home
-                </NavLink>
+                <NavLink end to="/" className="nav-link">Home</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/about" className="nav-link">
-                  About
-                </NavLink>
+                <NavLink to="/about" className="nav-link">About</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/services" className="nav-link">
-                  Services
-                </NavLink>
+                <NavLink to="/services" className="nav-link">Services</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/reviews" className="nav-link">
-                  Reviews
-                </NavLink>
+                <NavLink to="/reviews" className="nav-link">Reviews</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/contact" className="nav-link">
-                  Contact
-                </NavLink>
+                <NavLink to="/contact" className="nav-link">Contact</NavLink>
               </li>
             </ul>
           </div>
